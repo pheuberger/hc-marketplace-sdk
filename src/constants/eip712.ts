ff863465a4383a52ae1d1a64f1ee436c7130f34b
pheuberger <philipp@pheuberger.com>
2025-01-14 18:49:46 +0200

    chore: update domain name and version
    To bring the SDK to parity with our frontend. We explicitly use these
    values there.

--- src/constants/eip712.ts
+++ src/constants/eip712.ts
@@ -2,11 +2,11 @@
  * EIP712 domain name.
  * @see {@link https://eips.ethereum.org/EIPS/eip-712#definition-of-domainseparator EIP712 doc}
  */
-export const DOMAIN_NAME = "LooksRareProtocol";
+export const DOMAIN_NAME = "Hypercerts";
 
 /**
  * EIP712 domain version.
  * @see {@link https://eips.ethereum.org/EIPS/eip-712#definition-of-domainseparator EIP712 doc}
  */
-export const DOMAIN_VERSION = 2;
+export const DOMAIN_VERSION = 1;
 
